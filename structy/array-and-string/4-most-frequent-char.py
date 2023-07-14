2828f3250d96799c68cb700998ab2997c557f3ce
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-07-14 16:41:16 -0400

    most freq solved

--- structy/array-and-string/4-most-frequent-char.py
+++ structy/array-and-string/4-most-frequent-char.py
@@ -2,7 +2,46 @@
 
 # You can assume that the input string is non-empty.
 
-def most_frequent_char(s):
-    pass
+# input: string
+# output: most common letter
+# create hash map to count each letter in the string
+# create variable to hold letter and set to first letter
+# if letter is in hash map increment by 1
+# else add it to hash map
+# iterate through hash map using dict.items() which return view obj as tuples in list
+# set letter variable if value is higher than current letter
+# return letter
 
-most_frequent_char('bookeeper') # -> 'e'
+# time: O(n+m) because you are iterating through the string and hash map
+# space: O(n) because you are creating a hash map
+
+def most_frequent_char(s):
+    hash = dict()
+    letter = s[0]
+
+    for char in s:
+        if char not in hash:
+            hash[char] = 1
+        else:
+            hash[char] += 1
+
+    for key, value in hash.items():
+        if hash[letter] < value:
+            letter = key
+
+    return letter
+
+# you can also use Counter
+from collections import Counter
+
+def most_frequent_char_counter(s):
+    hash = Counter(s)
+    letter = s[0]
+
+    for key, value in hash.items():
+        if hash[letter] < value:
+            letter = key
+
+    return letter
+
+print(most_frequent_char('bookeeper')) # -> 'e'
