705798e9df602fb71ec9a50be54647d0017d9893
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-07-15 07:53:50 -0400

    pair sum added

---
+++ structy/array-and-string/5-pair-sum.py
@@ -0,0 +1,10 @@
+# Write a function, pair_sum, that takes in a list and a target sum as arguments. The function should return a tuple containing a pair of indices whose elements sum to the given target. The indices returned must be unique.
+
+# Be sure to return the indices, not the elements themselves.
+
+# There is guaranteed to be one such pair that sums to the target.
+
+def pair_sum(numbers, target_sum):
+    pass
+
+pair_sum([3, 2, 5, 4, 1], 8) # -> (0, 2)
