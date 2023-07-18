d75eb0790c731a96680000d08834b301e10218f5
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-07-18 17:14:58 -0400

    added intersection

---
+++ structy/array-and-string/7-intersection.py
@@ -0,0 +1,8 @@
+# Write a function, intersection, that takes in two lists, a,b, as arguments. The function should return a new list containing elements that are in both of the two lists.
+
+# You may assume that each input list does not contain duplicate elements.
+
+def intersection(a, b):
+    pass
+
+print(intersection([4,2,1,6], [3,6,9,2,10])) # -> [2,6]
