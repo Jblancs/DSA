9d7e418df86b001585e699ef9b9cf65510c219cf
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-07-19 13:39:59 -0400

    intersection solved

--- structy/array-and-string/7-intersection.py
+++ structy/array-and-string/7-intersection.py
@@ -2,7 +2,37 @@
 
 # You may assume that each input list does not contain duplicate elements.
 
+# input: 2 lists of numbers
+# output: list of all common numbers
+# create a set and store one of the lists into it
+# storing numbers in a set is O(1) time
+# for loop to iterate over second list to check if it is included in set
+# lookup is O(1) time
+# if it is in the set store the number in the common list
+# return common list
+
+# time O(n+m) because we store each num in list a set and iterate through list b to compare
+# spaace O(n) because we create a set and list
+
 def intersection(a, b):
-    pass
+    common = list()
+    set_a = set(a) # O(1) time to store each (similar to iterating and using .add)
+
+    for num in b:
+        # using the "in list" would make it n^2 but since its a set it is constant
+        if num in set_a: # O(1) time to lookup in set
+            common.append(num)
+
+    return common
+
+# better to solve with list comprehension
+def intersection_list_comp(a, b):
+    set_a = set(a)
+
+    return [num for num in b if num in set_a]
 
 print(intersection([4,2,1,6], [3,6,9,2,10])) # -> [2,6]
+
+# a = [ i for i in range(0, 50000) ]
+# b = [ i for i in range(0, 50000) ]
+# print(intersection(a, b)) # -> [0,1,2,3,..., 49999]
