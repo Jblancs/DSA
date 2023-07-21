00a5329c7faf418473da9cb2733e77361d4e2ca6
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-07-21 13:10:48 -0400

    linked list values updated

--- structy/linked-list/1-linked-list-values.py
+++ structy/linked-list/1-linked-list-values.py
@@ -1,6 +1,6 @@
 # Write a function, linked_list_values, that takes in the head of a linked list as an argument. The function should return a list containing all values of the nodes in the linked list.
 
-# Hey. This is our first linked list problem, so you should be liberal with watching the Approach and Walkthrough. Be productive! -AZ
+# Hey. This is our first linked list problem, so you should be liberal with watching the Approach and Walkthrough. 
 
 class Node:
   def __init__(self, val):
