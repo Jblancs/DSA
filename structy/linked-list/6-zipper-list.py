4638ec05db4b8f3e759bcc82bd2ab6f274881fd3
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-07-26 10:31:48 -0400

    zipper list solved

--- structy/linked-list/6-zipper-list.py
+++ structy/linked-list/6-zipper-list.py
@@ -4,13 +4,54 @@
 
 # You may assume that both input lists are non-empty.
 
+# input: heads of 2 linked lists
+# output: head of new zippered list
+# create current_1 = head_1.next to traverse first list
+# create current_2 = head_2 to traverse second list
+# create tail = head_1 variable to hold last node in zippered list
+# create count variable so you know which list to pull next node from
+# while loop to iterate until current_1 and current_2 is None
+# if count is even pull from list 2
+# tail.next = current_2 if count is even
+# reassign current_2 to current_2.next
+# increment count up one
+# repeat above steps for odds
+# if either current is None it will exit loop
+# if current_1 is None tail.next = current_1
+# repeat above step for current_2
+# this will handle if a list is longer than the other
+# return head_1
+
 class Node:
   def __init__(self, val):
     self.val = val
     self.next = None
 
 def zipper_lists(head_1, head_2):
-    pass
+    tail = head_1
+    current_1 = head_1.next
+    current_2 = head_2
+    count = 0
+
+    while current_1 is not None and current_2 is not None:
+      if count % 2 == 0:
+        tail.next = current_2
+        current_2 = current_2.next
+
+      else:
+        tail.next = current_1
+        current_1 = current_1.next
+
+      tail = tail.next
+      count += 1
+
+    if current_1 is not None:
+      tail.next = current_1
+    if current_2 is not None:
+      tail.next = current_2
+    return head_1
+
+
 
 a = Node("a")
 b = Node("b")
@@ -26,5 +67,5 @@ x.next = y
 y.next = z
 # x -> y -> z
 
-zipper_lists(a, x)
+print(zipper_lists(a, x))
 # a -> x -> b -> y -> c -> z
