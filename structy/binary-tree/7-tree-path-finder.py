3078314628da7513db7e932a15e74e11de58bb11
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-08-14 19:47:59 -0400

    added tree path finder

---
+++ structy/binary-tree/7-tree-path-finder.py
@@ -0,0 +1,34 @@
+# Write a function, path_finder, that takes in the root of a binary tree and a target value. The function should return an array representing a path to the target value. If the target value is not found in the tree, then return None.
+
+# You may assume that the tree contains unique values.
+
+class Node:
+  def __init__(self, val):
+    self.val = val
+    self.left = None
+    self.right = None
+
+def path_finder(root, target):
+  pass # todo
+
+
+a = Node("a")
+b = Node("b")
+c = Node("c")
+d = Node("d")
+e = Node("e")
+f = Node("f")
+
+a.left = b
+a.right = c
+b.left = d
+b.right = e
+c.right = f
+
+#      a
+#    /   \
+#   b     c
+#  / \     \
+# d   e     f
+
+path_finder(a, 'e') # -> [ 'a', 'b', 'e' ]
