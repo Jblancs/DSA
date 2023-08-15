9bce5f55ce91f2ebb2d6e2ea659e748db94b1f5d
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-08-15 16:37:40 -0400

    solved tree path finder

--- structy/binary-tree/7-tree-path-finder.py
+++ structy/binary-tree/7-tree-path-finder.py
@@ -2,14 +2,53 @@
 
 # You may assume that the tree contains unique values.
 
+# input: root and target val
+# output: list containing path to target or None if target not in tree
+# create base case if node is null return None
+# create base case if node.val == target return [root.val]
+# create left variable and recursive call
+# if left is not None append root.val to left and return left
+# same for right
+# if both are none return None
+
 class Node:
   def __init__(self, val):
     self.val = val
     self.left = None
     self.right = None
 
+# reorder list
 def path_finder(root, target):
-  pass # todo
+  result = _path_finder(root, target)
+  if result is None:
+    return None
+  else:
+    # reverses list 3rd arg is step
+    return result[::-1]
+
+def _path_finder(root, target):
+  if root == None:
+    return None
+
+  if root.val == target:
+    return [root.val]
+
+  left = _path_finder(root.left, target)
+  print(left)
+  if left is not None:
+    # *left takes list and unpacks items so it runs O(n) making it O(n^2)
+    # return [root.val, *left]
+    left.append(root.val)
+    return left
+
+  right = _path_finder(root.right, target)
+  if right is not None:
+    # return [root.val, *right]
+    # append will attach to end of list so need to reverse
+    right.append(root.val)
+    return right
+
+  return None
 
 
 a = Node("a")
@@ -31,4 +70,4 @@ c.right = f
 #  / \     \
 # d   e     f
 
-path_finder(a, 'e') # -> [ 'a', 'b', 'e' ]
+print(path_finder(a, 'e')) # -> [ 'a', 'b', 'e' ]
