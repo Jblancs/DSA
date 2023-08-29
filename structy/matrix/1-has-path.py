cf0fa4f0f77e6995aa9ee11d90f0738792ffa98d
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-08-28 20:36:39 -0400

    has path added

---
+++ structy/matrix/1-has-path.py
@@ -0,0 +1,17 @@
+# Write a function, has_path, that takes in a dictionary representing the adjacency list of a directed acyclic graph and two nodes (src, dst). The function should return a boolean indicating whether or not there exists a directed path between the source and destination nodes.
+
+# Hey. This is our first graph problem, so you should be liberal with watching the Approach and Walkthrough. Be productive, not stubborn. -AZ
+
+def has_path(graph, src, dst):
+  pass # todo
+
+graph = {
+  'f': ['g', 'i'],
+  'g': ['h'],
+  'h': [],
+  'i': ['g', 'k'],
+  'j': ['i'],
+  'k': []
+}
+
+has_path(graph, 'f', 'k') # True
