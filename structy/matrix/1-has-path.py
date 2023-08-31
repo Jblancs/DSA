8a1978277062acc8005e8e83bc3ade5404161b5b
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-08-30 20:58:53 -0400

    has path solved

--- structy/matrix/1-has-path.py
+++ structy/matrix/1-has-path.py
@@ -2,8 +2,48 @@
 
 # Hey. This is our first graph problem, so you should be liberal with watching the Approach and Walkthrough. Be productive, not stubborn. -AZ
 
+# input: adj list and 2 nodes
+# output: boolean if path between src node and dst node
+# create queue for a depth first trav starting with src
+# while queue has node iterate
+# create current by popleft
+# if current == dst return true
+# for loop to iterate and append neighbors
+# outside loop return false
+
+# acyclic mean no cycle
+
+from collections import deque
+
+# time: O(e) which is number of edges since we need to traverse through each edge
+# space: O(n) since we are added every node to queue
 def has_path(graph, src, dst):
-  pass # todo
+  queue = deque([src])
+
+  while queue:
+    current = queue.popleft()
+
+    if current == dst:
+      return True
+
+    for node in graph[current]:
+      queue.append(node)
+
+  return False
+
+# recursive solution
+def has_path_recur(graph, src, dst):
+  if src == dst:
+    return True
+
+  for node in graph[src]:
+    if has_path_recur(graph, node, dst) == True:
+      return True
+
+  return False
+
+
+
 
 graph = {
   'f': ['g', 'i'],
@@ -14,4 +54,5 @@ graph = {
   'k': []
 }
 
-has_path(graph, 'f', 'k') # True
+print(has_path(graph, 'f', 'k')) # True
+print(has_path_recur(graph, 'f', 'k')) # True
