62818c5b2739fffc42302de24f5e6500d49f9e9a
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-09-27 08:18:11 -0400

    added island count

---
+++ structy/matrix/6-island-count.py
@@ -0,0 +1,16 @@
+# Write a function, island_count, that takes in a grid containing Ws and Ls. W represents water and L represents land. The function should return the number of islands on the grid. An island is a vertically or horizontally connected region of land.
+
+def island_count(grid):
+  pass # todo
+
+
+grid = [
+  ['W', 'L', 'W', 'W', 'W'],
+  ['W', 'L', 'W', 'W', 'W'],
+  ['W', 'W', 'W', 'L', 'W'],
+  ['W', 'W', 'L', 'L', 'W'],
+  ['L', 'W', 'W', 'L', 'L'],
+  ['L', 'L', 'W', 'W', 'W'],
+]
+
+island_count(grid) # -> 3
