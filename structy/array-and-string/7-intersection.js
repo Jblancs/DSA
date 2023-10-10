b2e1e01c5d9e8cc73eb9104240e5abf26d3b86ee
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-10-10 11:13:37 -0400

    intersection solved js

--- structy/array-and-string/7-intersection.js
+++ structy/array-and-string/7-intersection.js
@@ -16,3 +16,13 @@ const intersection = (a, b) => {
   };
 
 console.log(intersection([4,2,1,6], [3,6,9,2,10])) // -> [2,6]
+
+console.log(intersection([2,4,6], [4,2])) // -> [2,4]
+
+const a = [];
+const b = [];
+for (let i = 0; i < 50000; i += 1) {
+  a.push(i);
+  b.push(i);
+}
+console.log(intersection(a, b)) // -> [0,1,2,3,..., 49999]
