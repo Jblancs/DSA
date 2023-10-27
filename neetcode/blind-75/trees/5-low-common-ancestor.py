4d7aa991094be9cdbd9c46110af01e3f18106a74
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2023-10-27 14:11:50 -0400

    update

--- neetcode/blind-75/trees/5-low-common-ancestor.py
+++ neetcode/blind-75/trees/5-low-common-ancestor.py
@@ -38,5 +38,6 @@ class TreeNode:
         self.left = None
         self.right = None
 
-class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+def lowestCommonAncestor(root, p, q):
+    pass 
