945bdddbd80e1994cbc2d8aa21ad12e5bd3ba1b0
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-01-24 11:04:37 -0500

    linked list solved

--- grokking/fast-slow-pointer/2-linked-list-cycle.py
+++ grokking/fast-slow-pointer/2-linked-list-cycle.py
@@ -1,9 +1,33 @@
+# input: head of linked list
+# output: boolean whether linked list contains cycle
+
+# initialize fast and slow pointer to head
+# while fast != null
+# if fast.data == slow.data return True
+# if fast.data == null return False
+# slow = slow.next
+# fast = fast.next.next
+
+# time = O(logn)
+# space = O(1)
 
 def detect_cycle(head):
+    if head is None:
+        return False
 
-   # Replace this placeholder return statement with your code
+    slow, fast = head, head
 
-   return False
+    while fast != None and fast.next:
+
+        slow = slow.next
+        fast = fast.next.next
+
+        if fast == slow:
+           return True
+
+    return False
+
+
 
 class LinkedListNode:
     # __init__ will be used to make a LinkedListNode type object.
@@ -62,3 +86,13 @@ class LinkedList:
                 result += ", "
         result += ""
         return result
+
+
+list_1 = LinkedList()
+list_1.create_linked_list([2,4,6,8,10])
+
+list_2 = LinkedList()
+list_2.create_linked_list([1,3,5,7,9])
+
+print(detect_cycle(list_1.head))
+print(detect_cycle(list_2.head))
