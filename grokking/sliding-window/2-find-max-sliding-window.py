ae03bd1756619cd16506ca00d83ae349a6bf1dc9
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-02-07 21:03:33 -0500

    find max slide solved

--- grokking/sliding-window/2-find-max-sliding-window.py
+++ grokking/sliding-window/2-find-max-sliding-window.py
@@ -1,5 +1,41 @@
+# input: list of nums and window size w
+# output: list of highest value in each window
+# initialize i = 0 for window
+# while loop until i + w < len(nums) + 1
+# initialize max to float('-inf')
+# for loop iterate through win_idx from range(i, i+w)
+# if nums[win_idx] > max reassign max to value
+# outside for loop: if result
+# if result[0] > max append max
+# else appendleft
+# else append max
+# i += 1
+# outside while loop return result
+
+from collections import deque
+
 def find_max_sliding_window(nums, w):
 
-    # Replace this placeholder return statement with your code
+    i = 0
+    result = deque()
 
-    return []
+    while (i + w) < (len(nums) + 1):
+        max = float('-inf')
+
+        for win_idx in range(i, i+w):
+            if nums[win_idx] > max:
+                max = nums[win_idx]
+
+        if result:
+            if result[0] > max:
+                result.append(max)
+            else:
+                result.appendleft(max)
+        else:
+            result.append(max)
+
+        i += 1
+
+    return result
+
+print(find_max_sliding_window([1,2,3,4,5,6,7,8,9,10], 3))
