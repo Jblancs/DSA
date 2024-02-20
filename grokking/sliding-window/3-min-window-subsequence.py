65df1c7bbe26bd8ad2ef9ad3a27f825536ddccc6
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-02-20 15:03:04 -0500

    min window subseq added

---
+++ grokking/sliding-window/3-min-window-subsequence.py
@@ -0,0 +1,7 @@
+# input: 2 strings
+# output: shortest substring in str1 such that str2 is a subseq of that substring
+# init variables to store length of each str
+# init min_sub_length to float("inf")
+# init 2 indexes to traverse both strings idx_str1 and idx_str2
+# init min_subseq to "" for storing result
+# 
