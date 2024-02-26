f7f5129967253fec167b5af62173902969338e79
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-02-26 10:51:41 -0500

    added time comp to array to bst

--- grokking/tree-depth-first-search/6-convert-sorted-array-to-bst.py
+++ grokking/tree-depth-first-search/6-convert-sorted-array-to-bst.py
@@ -5,10 +5,14 @@
 # init low to 0
 # init high to len(array)
 
+# time: O(n) where n is size of input array
+# space: O(logn) for recursive stack calls being the height of the tree
+
+
 from utils import TreeNode
 
 def sorted_array_to_bst_helper(nums, low, high):
-    if( low > high):
+    if(low > high):
         return None
 
     mid = low +(high-low)//2
