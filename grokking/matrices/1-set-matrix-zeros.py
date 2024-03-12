3aae04d9063d20200a1a4c6565c77e5cf20a1de8
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-03-12 19:21:55 -0400

    set matrix zero solved

---
+++ grokking/matrices/1-set-matrix-zeros.py
@@ -0,0 +1,40 @@
+# input: matrix
+# output: matrix with row/col containing 0 set to 0
+
+def set_matrix_zeros(mat):
+	rows = len(mat)
+	cols = len(mat[0])
+	fcol = False
+	frow = False
+
+	for i in range(rows):
+		if mat[i][0] == 0:
+			fcol = True
+
+	for i in range(cols):
+		if mat[0][i] == 0:
+			frow = True
+
+	for i in range(1, rows):
+		for j in range(1, cols):
+			if mat[i][j] == 0:
+				mat[0][j] = mat[i][0] = 0
+
+	for i in range(1, rows):
+		if mat[i][0] == 0:
+			for j in range(1, cols):
+				mat[i][j] = 0
+
+	for j in range(1, cols):
+		if mat[0][j] == 0:
+			for i in range(1, rows):
+				mat[i][j] = 0
+
+	if fcol:
+		for i in range(rows):
+			mat[i][0] = 0
+
+	if frow:
+		for j in range(cols):
+			mat[0][j] = 0
+	return mat
