e77ed9f6b78f45176b26268c50da83c272b92b1b
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-03-21 18:45:34 -0400

    rotate matrix solved

--- grokking/matrices/2-rotate-image.py
+++ grokking/matrices/2-rotate-image.py
@@ -3,7 +3,7 @@
 # input: matrix
 # output: matrix rotated 90 degrees clockwise
 # init n for length of matrix
-# create for loop to iterate row on range n // 2
+# create for loop to iterate row on range n // 2 since we only need to iterate over top half of matrix
 # create nested for loop col on range n - row - 1
 # swap top left cell with top right
 # swap top left with bottom right
@@ -13,5 +13,13 @@
 
 def rotate_image(matrix):
 
-    # Replace this placeholder return statement with your code
-    return [[]]
+    n = len(matrix)
+
+    for row in range(n//2):
+        for col in range(row, n - row - 1):
+
+            matrix[row][col], matrix[col][n - 1 - row] = matrix[col][n - 1 - row], matrix[row][col]
+            matrix[row][col], matrix[n - row - 1][n - col - 1] = matrix[n - row - 1][n - col - 1], matrix[row][col]
+            matrix[row][col], matrix[n - col - 1][row] = matrix[n - col - 1][row], matrix[row][col]
+
+    return matrix
