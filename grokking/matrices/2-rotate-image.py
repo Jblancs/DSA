30e2b10092320f77cde81beeede76f3810c7d099
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-03-21 17:58:32 -0400

    rotate image added

---
+++ grokking/matrices/2-rotate-image.py
@@ -0,0 +1,11 @@
+# Given n×n matrix, rotate the matrix 90 degrees clockwise. The performed rotation should be in place, i.e., the given matrix is modified directly without allocating another matrix.
+
+
+
+# input: matrix
+# output: matrix rotated 90 degrees clockwise
+
+def rotate_image(matrix):
+
+    # Replace this placeholder return statement with your code
+    return [[]]
