95531e39c368a6e45917a0abffe0bb7314997fa8
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-03-21 18:31:43 -0400

    psuedo code added

--- grokking/matrices/2-rotate-image.py
+++ grokking/matrices/2-rotate-image.py
@@ -1,9 +1,15 @@
 # Given n×n matrix, rotate the matrix 90 degrees clockwise. The performed rotation should be in place, i.e., the given matrix is modified directly without allocating another matrix.
 
-
-
 # input: matrix
 # output: matrix rotated 90 degrees clockwise
+# init n for length of matrix
+# create for loop to iterate row on range n // 2
+# create nested for loop col on range n - row - 1
+# swap top left cell with top right
+# swap top left with bottom right
+# swap top left with bottom left
+# return matrix
+
 
 def rotate_image(matrix):
 
