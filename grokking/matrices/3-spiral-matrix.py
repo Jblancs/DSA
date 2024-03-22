376456c17902a6df70b16ed0b9f986f31d897703
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-03-22 17:42:03 -0400

    spiral matric solved

--- grokking/matrices/3-spiral-matrix.py
+++ grokking/matrices/3-spiral-matrix.py
@@ -5,5 +5,23 @@
 
 def spiral_order(matrix):
 
-  # Replace this placeholder return statement with your code
-  return [-1]
+  rows, cols = len(matrix), len(matrix[0])
+  row, col = 0, -1
+  direction = 1
+  result = []
+
+  while rows > 0 and cols > 0:
+    for _ in range(cols):
+      col += direction
+      result.append(matrix[row][col])
+
+    rows -= 1
+
+    for _ in range(rows):
+      row += direction
+      result.append(matrix[row][col])
+
+    cols -= 1
+    direction *= -1
+
+  return result
