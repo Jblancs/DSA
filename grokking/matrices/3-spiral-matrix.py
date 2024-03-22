89a595eb1cf5268627f06ea4cbca249443b79ed3
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-03-22 17:16:07 -0400

    spiral matrix added

---
+++ grokking/matrices/3-spiral-matrix.py
@@ -0,0 +1,9 @@
+# Given an m×n matrix, return an array containing the matrix elements in spiral order, starting from the top-left cell.
+
+# input: matrix
+# output: array with values in spiral order
+
+def spiral_order(matrix):
+
+  # Replace this placeholder return statement with your code
+  return [-1]
