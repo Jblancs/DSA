405b292f686b001e48b3afbf550284c7f1e4fa4d
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-03-28 19:02:22 -0400

    where will ball fall added

---
+++ grokking/matrices/4-where-will-ball-fall.py
@@ -0,0 +1,9 @@
+# input: n for number of ball and grid
+# output: array size n with ith element indiccating column that ball falls out of or -1 if stuck
+
+# 
+
+def find_exit_column(grid):
+
+    # Replace this placeholder return statement with your code
+    return []
