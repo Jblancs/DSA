7a1ae5aae55a67bdafcef0cdf738bb8f68044e65
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-04-05 18:52:22 -0400

    rotate image re do

--- grokking/matrices/2-rotate-image.py
+++ grokking/matrices/2-rotate-image.py
@@ -15,13 +15,4 @@
 
 def rotate_image(matrix):
 
-    n = len(matrix)
-
-    for row in range(n//2):
-        for col in range(row, n - row - 1):
-
-            matrix[row][col], matrix[col][n - 1 - row] = matrix[col][n - 1 - row], matrix[row][col]
-            matrix[row][col], matrix[n - row - 1][n - col - 1] = matrix[n - row - 1][n - col - 1], matrix[row][col]
-            matrix[row][col], matrix[n - col - 1][row] = matrix[n - col - 1][row], matrix[row][col]
-
-    return matrix
+    
