f7302c07aac95c7415be491fda4047ec4dfa54dc
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-04-05 19:31:54 -0400

    corrected error on rotate image

--- grokking/matrices/2-rotate-image.py
+++ grokking/matrices/2-rotate-image.py
@@ -20,7 +20,7 @@ def rotate_image(matrix):
     for row in range(n // 2):
         for col in range(row, n - 1 - row):
             matrix[row][col], matrix[col][n - 1 - row] = matrix[col][n - 1 - row], matrix[row][col]
-            matrix[row][col], matrix[n - 1 - row][n - 1 - col] = matrix[n - row][n - 1 - col], matrix[row][col]
+            matrix[row][col], matrix[n - 1 - row][n - 1 - col] = matrix[n - 1 - row][n - 1 - col], matrix[row][col]
             matrix[row][col], matrix[n - 1 - col][row] = matrix[n - 1 - col][row], matrix[row][col]
 
     return matrix
