17e7fb46437ffa77136733d35523c4e9cf6f2093
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-04-05 19:33:43 -0400

    spiral matrix removed

--- grokking/matrices/3-spiral-matrix.py
+++ grokking/matrices/3-spiral-matrix.py
@@ -5,23 +5,4 @@
 
 def spiral_order(matrix):
 
-  rows, cols = len(matrix), len(matrix[0])
-  row, col = 0, -1
-  direction = 1
-  result = []
 
-  while rows > 0 and cols > 0:
-    for _ in range(cols):
-      col += direction
-      result.append(matrix[row][col])
-
-    rows -= 1
-
-    for _ in range(rows):
-      row += direction
-      result.append(matrix[row][col])
-
-    cols -= 1
-    direction *= -1
-
-  return result
