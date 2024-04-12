45373ab1e91fd6f0ff6d23410cf1ebeb152e42d1
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-04-12 12:37:13 -0400

    psudo code for flatten binary tree

--- grokking/tree-depth-first-search/1-binary-tree-to-LL.py
+++ grokking/tree-depth-first-search/1-binary-tree-to-LL.py
@@ -1,5 +1,17 @@
 from utils import TreeNode
 
+# edge case if root is None return
+# init current to root
+# while loop to iterate while current is exists
+# if current.left exists find the right most node of the left subtree
+# init last to current.left
+# while last.right to iterate until right most node
+# last = last.right
+# assign last.right to current.right
+# assign current.right to current.left
+# assign current.left to None
+# outside if statement we reassign current to current.right
+# return root
+
 
 def flatten_tree(root):
-
