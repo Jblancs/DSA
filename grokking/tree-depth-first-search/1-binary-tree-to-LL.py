7a5838a9626845cbe7ebb2669f29c76c64c39526
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-04-12 10:49:28 -0400

    flatten binary tree removed

--- grokking/tree-depth-first-search/1-binary-tree-to-LL.py
+++ grokking/tree-depth-first-search/1-binary-tree-to-LL.py
@@ -2,22 +2,4 @@ from utils import TreeNode
 
 
 def flatten_tree(root):
-    if not root:
-        return
 
-    current = root
-
-    while current:
-        if current.left:
-            last = current.left
-
-            while last.right:
-                last = last.right
-
-            last.right = current.right
-            current.right = current.left
-            current.left = None
-
-        current = current.right
-
-    return root
