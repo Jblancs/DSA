0cfc09f9735e006ff207691ebe80c202ca8aec23
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-04-17 19:26:59 -0400

    deserialize removed

--- grokking/tree-depth-first-search/2-diameter-of-binary-tree.py
+++ grokking/tree-depth-first-search/2-diameter-of-binary-tree.py
@@ -1,4 +1,24 @@
-def diameter_helper(node, diameter):
+# init diameter to 0
+# create helper function that takes in root and diam
+# unpack function to _, diameter
+# in helper function
+# 
 
+def diameter_helper(node, diameter):
+    if node is None:
+        return 0, diameter
+
+    else:
+        lh, diameter = diameter_helper(node.left, diameter)
+        rh, diameter = diameter_helper(node.right, diameter)
+
+        diameter = max(diameter, lh + rh)
+
+        return max(lh, rh) + 1, diameter
 
 def diameter_of_binaryTree(root):
+    diameter = 0
+
+    _, diameter = diameter_helper(root, diameter)
+
+    return
