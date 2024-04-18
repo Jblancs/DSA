5e5099871d8e6973b043483199f460b230e77124
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-04-17 20:34:03 -0400

    deseriel re-solved

--- grokking/tree-depth-first-search/3-serial-deserial-binary-tree.py
+++ grokking/tree-depth-first-search/3-serial-deserial-binary-tree.py
@@ -4,12 +4,41 @@ MARKER = "M"
 m = 1
 
 def serialize_rec(node, stream):
+    global m
+
+    if node is None:
+        stream.append(MARKER + str(m))
+        m += 1
+        return
+
+    stream.append(node.data)
+
+    serialize_rec(node.left, stream)
+    serialize_rec(node.right, stream)
 
 
 def serialize(root):
+    stream = []
+
+    serialize_rec(root, stream)
+    return stream
 
 
 def deserialize_helper(stream):
+    val = stream.pop()
+
+    if type(val) is str and val[0] == MARKER:
+        return None
+
+    node = TreeNode(val)
+
+    node.left = deserialize_helper(stream)
+    node.right = deserialize_helper(stream)
+
+    return node
 
 
 def deserialize(stream):
+    stream.reverse()
+    node = deserialize_helper(stream)
+    return node
