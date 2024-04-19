3f8d93f60cb5b865544588e321c2e0ffbedd1348
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-04-19 18:09:21 -0400

    invert binary tree removed

--- grokking/tree-depth-first-search/4-invert-binary-tree.py
+++ grokking/tree-depth-first-search/4-invert-binary-tree.py
@@ -1,16 +1,4 @@
 
 
 def mirror_binary_tree(root):
-    if root is None:
-        return None
 
-    if root.left:
-        left = mirror_binary_tree(root.left)
-
-    if root.right:
-        right = mirror_binary_tree(root.right)
-
-    root.left = right
-    root.right = left
-
-    return root
