1f8871e0183cc5e9d23f647b069a679904bd9786
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-04-19 18:56:56 -0400

    max path sum reslved

--- grokking/tree-depth-first-search/5-binary-tree-max-path-sum.py
+++ grokking/tree-depth-first-search/5-binary-tree-max-path-sum.py
@@ -15,17 +15,35 @@
 # if root.right total += root.right
 # if total > sum: sum = total
 
-def max_contrib(root, sum):
+def max_contrib(root):
     if root is None:
         return 0
 
+    max_left = max_contrib(root.left)
+    max_right = max_contrib(root.right)
+
+    left_tree = 0
+    right_tree = 0
+
+    if max_left > 0:
+        left_tree = max_left
+
+    if max_right > 0:
+        right_tree = max_right
+
+    new_path_value = root.data + left_tree + right_tree
+
+    max_sum = max_contrib.max_sum
+    max_contrib.max_sum = max(max_sum, new_path_value)
+
+    return root.data + max(left_tree, right_tree)
 
 
 
 
 def max_path_sum(root):
-    sum = float("-inf")
+    max_contrib.max_sum = float("-inf")
 
-    max_contrib(root, sum)
+    max_contrib(root)
 
     return sum
