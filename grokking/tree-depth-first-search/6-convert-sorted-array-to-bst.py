d7cc531aedd07dac381b40cda8d5996bcd746c02
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-04-26 21:39:11 -0400

    convert sorted array resolved

--- grokking/tree-depth-first-search/6-convert-sorted-array-to-bst.py
+++ grokking/tree-depth-first-search/6-convert-sorted-array-to-bst.py
@@ -12,9 +12,26 @@
 from utils import TreeNode
 
 def sorted_array_to_bst_helper(nums, low, high):
+    if low > high:
+        return None
+
+    mid = low + (high - low)//2
+    root = TreeNode(nums[mid])
+
+    root.left = sorted_array_to_bst_helper(nums, low, mid - 1)
+    root.right = sorted_array_to_bst_helper(nums, mid + 1, high)
+
+    return root
+
 
 
 
 def sorted_array_to_bst(nums):
+    low = 0
+    high = len(nums) - 1
+
+    res = sorted_array_to_bst_helper(nums, low, high)
+
+    return res
 
 print(sorted_array_to_bst([11,22,33,44,55,66,77,88]))
