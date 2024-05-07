0f09266e995fbaebf42686c3c11af7caf9994d8d
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-05-07 17:24:14 -0400

    validate bst solved

--- grokking/tree-depth-first-search/10-validate-bst.py
+++ grokking/tree-depth-first-search/10-validate-bst.py
@@ -1,6 +1,23 @@
 import math
 
 def validate_bst_rec(root, prev):
+    if not root:
+        return True
+
+    if not validate_bst_rec(root.left, prev):
+        return False
+
+    if root.data <= prev[0]:
+        return False
+
+    prev[0] = root.data
+
+    return validate_bst_rec(root.right, prev)
+
+
 
 
 def validate_bst(root):
+    prev = [-math.inf]
+
+    return validate_bst_rec(root, prev)
