d8f9480c6afd0bbe3df1b366db4ab441782d5b9b
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-05-08 18:19:06 -0400

    valid palindrome removed

--- grokking/two-pointer/1-valid-palindrome.py
+++ grokking/two-pointer/1-valid-palindrome.py
@@ -5,17 +5,6 @@
 
 def is_palindrome(s):
 
-  point_a = 0
-  point_b = len(s) - 1
-
-  while point_a < point_b:
-    if s[point_a] == s[point_b]:
-      point_a += 1
-      point_b -= 1
-    else:
-      return False
-
-  return True
 
 print(is_palindrome("kayak"))
 print(is_palindrome("hello"))
