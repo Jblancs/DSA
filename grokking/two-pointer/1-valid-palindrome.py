d3d99d317eb0137277b28e3178a64054b3da783a
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-05-08 18:19:31 -0400

    valid palindrome re solved

--- grokking/two-pointer/1-valid-palindrome.py
+++ grokking/two-pointer/1-valid-palindrome.py
@@ -4,7 +4,17 @@
 # space: O1
 
 def is_palindrome(s):
+  left = 0
+  right = len(s) - 1
 
+  while left < right:
+    if s[left] != s[right]:
+      return False
+
+    left += 1
+    right -= 1
+
+  return True
 
 print(is_palindrome("kayak"))
 print(is_palindrome("hello"))
