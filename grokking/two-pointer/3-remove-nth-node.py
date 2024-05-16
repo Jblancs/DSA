5cb000efdeac7fd26f73e43f0aa83b980fc1d2a8
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-05-16 14:06:55 -0400

    remove nth node re solved

--- grokking/two-pointer/3-remove-nth-node.py
+++ grokking/two-pointer/3-remove-nth-node.py
@@ -51,7 +51,20 @@ class LinkedList:
 
 
 def remove_nth_last_node(head, n):
+    left = head
+    right = head
 
+    for i in range(n):
+        right = right.next
+
+    if not right:
+        return head.next
+
+    while right.next:
+        left = left.next
+        right = right.next
+
+    left.next = left.next.next
 
     return head
 
