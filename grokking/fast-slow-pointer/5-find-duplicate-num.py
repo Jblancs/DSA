0e0b864982889c31c4c3be60c5552782b48588cc
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-05-30 19:07:22 -0400

    duplicate number removed

--- grokking/fast-slow-pointer/5-find-duplicate-num.py
+++ grokking/fast-slow-pointer/5-find-duplicate-num.py
@@ -14,22 +14,7 @@
 
 
 def find_duplicate(nums):
-
-    fast = slow = nums[0]
-    while True:
-        slow = nums[slow]
-        fast = nums[nums[fast]]
-
-        if slow == fast:
-            break
-
-    slow = nums[0]
-
-    while fast != slow:
-        slow = nums[slow]
-        fast = nums[fast]
-
-    return fast
+    return
 
 print(find_duplicate([3,4,4,4,2]))
 print(find_duplicate([1,1]))
