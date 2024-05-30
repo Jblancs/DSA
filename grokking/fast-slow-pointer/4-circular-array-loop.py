c386e70e2efb7a00cbf3b6cbc8af831004eb83cb
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-05-30 18:42:45 -0400

    circular loop solved

--- grokking/fast-slow-pointer/4-circular-array-loop.py
+++ grokking/fast-slow-pointer/4-circular-array-loop.py
@@ -31,13 +31,41 @@
 # space: O(1)
 
 def next_step(pointer, value, size):
-    return
+    result = (pointer + value) % size
+    if result < 0:
+        result += size
+    return result
 
 def is_not_cycle(nums, prev_direct, pointer):
-    return
+    curr_direct = nums[pointer] >= 0
+    if (prev_direct != curr_direct) or (abs(nums[pointer] % len(nums)) == 0):
+        return True
+    else:
+        return False
 
 def circular_array_loop(nums):
+    size = len(nums)
 
+    for i in range(size):
+        slow = fast = i
+
+        forward = nums[i] > 0
+
+        while True:
+            slow = next_step(slow, nums[slow], size)
+            if is_not_cycle(nums, forward, slow):
+                break
+
+            fast = next_step(fast, nums[fast], size)
+            if is_not_cycle(nums, forward, fast):
+                break
+
+            fast = next_step(fast, nums[fast], size)
+            if is_not_cycle(nums, forward, fast):
+                break
+
+            if slow == fast:
+                return True
     return False
 
 print(circular_array_loop([1,3,-2,-4,1]))
