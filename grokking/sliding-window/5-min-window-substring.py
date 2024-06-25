1492a91133bf079ebcea3bf27d46c3787bf70dc5
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-06-25 06:19:01 -0400

    min window substring removed

--- grokking/sliding-window/5-min-window-substring.py
+++ grokking/sliding-window/5-min-window-substring.py
@@ -12,47 +12,8 @@
 
 def min_window(s, t):
 
-    min_window_substring = [-1,-1]
-    min_window_len = float("inf")
-    window = {}
-    req_count = {}
 
-    for char in t:
-        if char not in req_count:
-            req_count[char] = 1
-            window[char] = 0
-        else:
-            req_count[char] += 1
-
-    current = 0
-    required = len(req_count)
-    start = 0
-
-    for end in range(len(s)):
-        char = s[end]
-
-        if char in t:
-            window[char] += 1
-
-        if char in t and window[char] == req_count[char]:
-            current += 1
-
-        while current == required:
-
-            if (end - start + 1) < min_window_len:
-                min_window_len = (end - start + 1)
-                min_window_substring = [start, end]
-
-            if s[start] in t:
-                window[s[start]] -= 1
-
-            if s[start] in req_count and window[s[start]] < req_count[s[start]]:
-                current -= 1
-
-            start += 1
-    start, end = min_window_substring
-    res = s[start:end+1] if min_window_len != float("infinity") else ""
-    return res
+    return 
 
 
 print(min_window("ABDFGDCKAB" , "ABCD"))
