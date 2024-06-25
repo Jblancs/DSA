ff39f921678a63926e5c6f295b9fc717caa43bff
Jordan Blancaflor <jordan.blancaflor@gmail.com>
2024-06-24 22:30:47 -0400

    long repeat solved

--- grokking/sliding-window/4-long-repeating-char-replace.py
+++ grokking/sliding-window/4-long-repeating-char-replace.py
@@ -18,9 +18,27 @@
 # space O(1) for hashmap with a max storing is 26 letters
 
 def longest_repeating_character_replacement(s, k):
+    string_len = len(s)
+    len_of_max_sub = 0
+    start = 0
+    char_freq = {}
+    most_freq_char = 0
 
+    for end in range(string_len):
+        if s[end] not in char_freq:
+            char_freq[s[end]] = 1
+        else:
+            char_freq[s[end]] += 1
 
-    return 
+        most_freq_char = max(most_freq_char, char_freq[s[end]])
+
+        if end - start + 1 - most_freq_char > k:
+            char_freq[s[start]] -= 1
+            start += 1
+
+        length_of_max_sub = max(end - start + 1, length_of_max_sub)
+
+    return length_of_max_sub
 
 
 
